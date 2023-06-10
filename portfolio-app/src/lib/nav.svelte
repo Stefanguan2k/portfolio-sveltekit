3e69ab6a27d7e6396f13faa7d31ccd24fd7787cb
Stefanguan2k <stefanguan2k@gmail.com>
2023-06-10 11:53:50 -0700

    Added snap scroll and navbar anchors

--- portfolio-app/src/lib/nav.svelte
+++ portfolio-app/src/lib/nav.svelte
@@ -11,13 +11,13 @@
 
 <nav
   class="fixed bottom-0 left-[50%] translate-x-[-50%] mb-6 w-[90%] text-zinc-100
-    md:left-0 md:top-[50%] md:translate-y-[-50%] md:translate-x-0 md:h-[95%]"
+    md:left-0 md:top-[50%] md:translate-y-[-50%] md:translate-x-0 md:h-[95%] md:w-max"
   bind:this={navbar}
   on:mouseover={toggleNav}
   on:focus={toggleNav}
   data-expanded="false"
 >
-  <ul class="relative flex gap-16 p-6 justify-evenly md:flex-col z-50">
+  <ul class="relative flex gap-16 justify-evenly p-6 md:flex-col md:gap-[7vh] z-50">
     <li class="hidden md:block">
       <a href="/"
         ><img
@@ -27,8 +27,8 @@
         /></a
       >
     </li>
-    <li>
-      <a href="/" class="flex gap-12"
+    <li class="relative">
+      <a href="/#hero" class="flex gap-12 w-max"
         ><img
           src="../src/assets/svg/house-solid.svg"
           alt="Home Screen"
@@ -36,9 +36,12 @@
         />
         <span class="hidden md:data-[active='true']:block">Home</span></a
       >
+      <div class="hidden absolute h-10 w-1.5 bg-main-100 rounded-xl top-[50%] translate-y-[-50%] -right-5 md:block">
+        <!-- Active indicator -->
+      </div>
     </li>
     <li>
-      <a href="/"
+      <a href="/#projects" class="flex gap-12 w-max"
         ><img
           src="../src/assets/svg/library.svg"
           alt="Projects"
@@ -47,7 +50,7 @@
       >
     </li>
     <li>
-      <a href="/"
+      <a href="/#aboutme" class="flex gap-12 w-max"
         ><img
           src="../src/assets/svg/user-solid.svg"
           alt="About Me"
@@ -56,7 +59,7 @@
       >
     </li>
     <li>
-      <a href="/"
+      <a href="/#contact" class="flex gap-12 w-max"
         ><img
           src="../src/assets/svg/envelope-solid.svg"
           alt="Contact Me"
@@ -67,8 +70,8 @@
   </ul>
 
   <div
-    class="absolute bg-nav-100 backdrop-blur-lg h-full w-full top-0 rounded-xl
-    md:rounded-bl-none md:rounded-tl-none md:w-80 md:-translate-x-[calc(320px-72px)] md:hover:translate-x-0 transition-all"
+    class="absolute bg-nav-100 backdrop-blur-lg h-full w-full top-0 rounded-3xl
+    md:rounded-bl-none md:rounded-tl-none md:rounded-tr-xl md:rounded-br-xl md:w-80 md:-translate-x-[calc(320px-72px)] md:hover:translate-x-0 transition-all"
   />
 </nav>
 
--- portfolio-app/src/routes/+page.svelte
+++ portfolio-app/src/routes/+page.svelte
@@ -4,28 +4,35 @@
 
 <!-- bg-background-100 -->
 
-<div class="flex flex-col gap-8 text-white">
+<div
+  class="flex flex-col gap-8 text-white snap-y snap-mandatory h-screen overflow-y-scroll scroll-smooth"
+>
   <section
-    class="flex justify-center items-center h-[90vh] mt-4 p-4 bg-background-500 rounded-2xl
+    class="flex justify-center items-center min-h-[90vh] mt-4 p-10 bg-background-500 rounded-2xl snap-start scroll-mt-11
     md:ml-24"
+    id="hero"
   >
-    <p>Hi, I am Stefan Guan</p>
+    <h1>Hi, I am Stefan Guan</h1>
   </section>
   <section
-    class="h-[90vh] p-4 bg-background-500 rounded-2xl
+    class="min-h-[90vh] p-10 bg-background-500 rounded-2xl snap-start scroll-mt-11
     md:ml-24"
+    id="projects"
   >
-    <p>Lorem Ipsum</p>
+    <h2 class="text-2xl font-bold">My Projects</h2>
   </section>
-  
-
-  <!-- <section
-    class="bg-background-500 mt-6
+  <section
+    class="min-h-[90vh] p-10 bg-background-500 rounded-2xl snap-start scroll-mt-11
     md:ml-24"
+    id="aboutme"
   >
-    <h1>Welcome to SvelteKit</h1>
-    <p>
-      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-    </p>
-  </section> -->
+    <h2 class="text-2xl font-bold">About Me</h2>
+  </section>
+  <section
+    class="min-h-[90vh] p-10 bg-background-500 rounded-2xl snap-start scroll-mt-11 mb-16
+    md:ml-24"
+    id="contact"
+  >
+    <h2 class="text-2xl font-bold">Contact Me</h2>
+  </section>
 </div>
