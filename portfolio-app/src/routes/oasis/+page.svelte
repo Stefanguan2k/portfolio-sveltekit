04957a0191348fc7b93e2404d5b3330b8e4f7ba4
Stefanguan2k <stefanguan2k@gmail.com>
2023-06-20 16:38:54 -0700

    Added additional images to oasis page

--- portfolio-app/src/routes/oasis/+page.svelte
+++ portfolio-app/src/routes/oasis/+page.svelte
@@ -398,13 +398,29 @@
       classList=""
     />
     <p class="text-zinc-700 dark:text-zinc-300 max-w-[800px]">
-      After creating the screens for the main user flow, I linked them up to
-      create an interactive low fidelity prototype. The prototype enabled
-      testing via usability studies.
+      With the basic skeleton of the website completed, the next step was to
+      create paper wireframes. This allowed quickly iterate through ideas for
+      how the would look.
     </p>
     <ImgContainer
-      path="../src/assets/img/sv-lofi-proto.webp"
-      alt="Low fidelity prototype of the app"
+      path="../src/assets/img/oasis-paper-wireframe.webp"
+      alt="Paper wireframe of the oasis website"
+      classList=""
+    />
+    <p class="text-zinc-700 dark:text-zinc-300 max-w-[800px]">
+      After satisfactory paper wireframes, I converted the designs into digital
+      wireframes using Adobe XD. I created a pair of mobile and desktop
+      wireframes. Thus, the digital wireframes enabled the ability to gether
+      more feedback.
+    </p>
+    <ImgContainer
+      path="../src/assets/img/oasis-digital-wireframe.webp"
+      alt="Digital wireframe of the oasis website"
+      classList=""
+    />
+    <ImgContainer
+      path="../src/assets/img/oasis-digital-wireframe-mobile.webp"
+      alt="Digital wireframe of the mobile oasis website"
       classList=""
     />
   </section>
@@ -418,8 +434,8 @@
     <p class="text-zinc-700 dark:text-zinc-300 max-w-[800px]">
       After connecting the wireframes into a low-fidelity prototype, I set out
       to test it with a usability study. The parameters of the study include:
-      (1) An unmoderated usability study, (2) remote study in Canada, (3) six
-      particiapnts, and (4) a duration of 40 minutes. The data from the
+      (1) An unmoderated usability study, (2) remote study in Canada, (3) five
+      particiapnts, and (4) a duration of 20-40 minutes. The data from the
       usability study gave three important insights.
     </p>
     <!-- Insight container -->
@@ -430,19 +446,18 @@
         <div class="flex items-center gap-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="1em"
             viewBox="0 0 512 512"
-            class="w-6 h-6 fill-[#308266] dark:fill-[#90c7b4]"
-            ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M448 160H320V128H448v32zM48 64C21.5 64 0 85.5 0 112v64c0 26.5 21.5 48 48 48H464c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zM448 352v32H192V352H448zM48 288c-26.5 0-48 21.5-48 48v64c0 26.5 21.5 48 48 48H464c26.5 0 48-21.5 48-48V336c0-26.5-21.5-48-48-48H48z"
-            /></svg
+            class="w-6 h-6 fill-[hsl(260,70%,60%)] dark:fill-[hsl(260,37%,64%)]"
           >
-          <div class="text-lg">Confusing Tracker</div>
+            <path
+              d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
+            />
+          </svg>
+
+          <div class="text-lg">Saved Payment Methods</div>
         </div>
         <p>
-          Users found the progress tracker during voter registration confusing.
-          They also found the accessibility button a bit confusing as it is
-          similar to the progress tracker.
+          Users wanted a way select a previously used card for future bookings.
         </p>
       </div>
       <div
@@ -451,18 +466,18 @@
         <div class="flex items-center gap-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-            class="w-6 h-6 fill-[#308266] dark:fill-[#90c7b4]"
-            ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"
-            /></svg
+            viewBox="0 0 448 512"
+            class="w-6 h-6 fill-[hsl(260,70%,60%)] dark:fill-[hsl(260,37%,64%)]"
           >
-          <div class="text-lg">Support</div>
+            <path
+              d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+            />
+          </svg>
+          <div class="text-lg">Navigation</div>
         </div>
         <p>
-          The users wanted a way to contact support during registration in case
-          they have questions or concerns.
+          The current navigation bar do not sufficiently describe where an
+          element links to.
         </p>
       </div>
       <div
@@ -471,18 +486,18 @@
         <div class="flex items-center gap-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 640 512"
-            class="w-6 h-6 fill-[#308266] dark:fill-[#90c7b4]"
-            ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z"
-            /></svg
+            viewBox="0 0 512 512"
+            class="w-6 h-6 fill-[hsl(260,70%,60%)] dark:fill-[hsl(260,37%,64%)]"
           >
-          <div class="text-lg">Election Candidates</div>
+            <path
+              d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
+            />
+          </svg>
+          <div class="text-lg">Contrast</div>
         </div>
         <p>
-          Users needed a way to quickly research about candidates that are
-          participating in a specific election to better inform their decisions.
+          Elements there were toggleable lacked contrast. Some users reported
+          not konwing whether an option was on or off.
         </p>
       </div>
     </div>
